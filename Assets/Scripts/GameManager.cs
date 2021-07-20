a094f8427e192f44abbc81000ce2e98a677f8bc7
labkungen <87672689+labkungen@users.noreply.github.com>
2021-07-20 23:15:57 +0200

    Prepared menus and persistence
    Inital work on creating menu scene and persistence routines in the gamemanager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public string playerName;
+
+    public int[] highscores = {0,0,0};
+    public string[] highscoreNames = {"STILL", "NOT", "TAKEN"};
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadHighscores();
+        
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public int[] highscores;
+        public string[] highscoreNames;
+    }
+
+    public void SaveHighscores()
+    {
+        SaveData data = new SaveData();
+        data.highscores = highscores;
+        data.highscoreNames = highscoreNames;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadHighscores()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            highscores = data.highscores;
+            highscoreNames = data.highscoreNames;
+        }
+    }
+}
